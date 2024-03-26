22332429bfc2bddf9e8c7262a4fea9a717d0a000
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-03-26 17:27:22 +0100

    add: alert when the km or the age are not a number or they are negative

--- js/script.js
+++ js/script.js
@@ -22,23 +22,27 @@ sendBtn.addEventListener("click", function() {
     const ageInputValue = parseInt(ageInput.value); //* number
     console.log("il passeggero ha " + ageInputValue + " anni");
 
-    //? calcolo biglietto intero
-    const price = kmCost * kmInputValue; //* number
-    console.log("il prezzo del biglietto è di € " + price);
+    if (!isNaN(kmInputValue) && !isNaN(ageInput) && kmInputValue > 0 && ageInputValue > 0) {
+        //? calcolo biglietto intero
+        const price = kmCost * kmInputValue; //* number
+        console.log("il prezzo del biglietto è di € " + price);
 
-    //? condizione discount
-    let discount = 0; //* number
+        //? condizione discount
+        let discount = 0; //* number
 
-    if (ageInputValue < 18) {
-        discount = 20;
-    } else if (ageInputValue > 65) {
-        discount = 40;
+        if (ageInputValue < 18) {
+            discount = 20;
+        } else if (ageInputValue > 65) {
+            discount = 40;
+        }
+
+        const totalDiscount = (price * discount) / 100 //* number
+        console.log("ha diritto ad uno scnto di € " + totalDiscount);
+
+        //? prezzo finale
+        const finalPrice = price - totalDiscount //* number
+        console.log("il totale compreso di sconto è di € " + finalPrice);
+    } else {
+        alert("DATI ERRATI, INSERIRE DEI DATI VALIDI!")
     }
-
-    const totalDiscount = (price * discount) / 100 //* number
-    console.log("ha diritto ad uno scnto di € " + totalDiscount);
-
-    //? prezzo finale
-    const finalPrice = price - totalDiscount //* number
-    console.log("il totale compreso di sconto è di € " + finalPrice);
 })
